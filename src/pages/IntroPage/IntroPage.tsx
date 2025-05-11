19e6ff3162326c110a9f7d4f7d41feecc8c4ac4e
CAFETO <43516663+YeongjunHwang@users.noreply.github.com>
2025-05-12 01:05:53 +0900

    로티 이미지 추가

--- src/pages/IntroPage/IntroPage.tsx
+++ src/pages/IntroPage/IntroPage.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Lottie from 'lottie-react';
 import { useNavigate } from 'react-router-dom';
-import { MdFastfood } from 'react-icons/md';
 import FoodBanner from '../../components/FoodBanner';
 import AdvertiseBanner from '../../components/AdvertiseBanner';
+import introAnim from './IntroData/Intro.json';
+
 
 import './IntroPage.css';
 
@@ -13,7 +15,12 @@ const IntroPage: React.FC = () => {
     <div className="intro-page">
       <FoodBanner />
       <div className="intro-content">
-        <h1><MdFastfood className="intro-icon" /> 오늘 뭐 먹지?</h1>
+        <h1>
+          <span className="intro-icon-lottie">
+            <Lottie animationData={introAnim} loop={true} />
+          </span>
+          오늘 뭐 먹지?
+        </h1>
         <p className="subtitle">매일 반복되는 고민, 저희가 해결해드릴게요</p>
         <button className="start-btn" onClick={() => navigate('/game')}>
           추천받기 시작

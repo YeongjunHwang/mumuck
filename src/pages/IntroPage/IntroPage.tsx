ca83e7cd002374e7f6b5cec70ce2a25a4af0d721
CAFETO <43516663+YeongjunHwang@users.noreply.github.com>
2025-05-14 20:03:16 +0900

    구글 에드센스 광고 삽입

--- src/pages/IntroPage/IntroPage.tsx
+++ src/pages/IntroPage/IntroPage.tsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Lottie from 'lottie-react';
 import { useNavigate } from 'react-router-dom';
 import FoodBanner from '../../components/FoodBanner';
 import AdvertiseBanner from '../../components/AdvertiseBanner';
 import introAnim from './IntroData/Intro.json';
 import SiteInfo from '../../components/SiteInfo/SiteInfo';
-
-
-
 import './IntroPage.css';
+
+
 
 const IntroPage: React.FC = () => {
   const navigate = useNavigate();
-
+  useEffect(() => {
+    const ads = document.getElementsByClassName('adsbygoogle');
+    const ad = ads[0];
+    if (ad && !ad.getAttribute('data-adsbygoogle-status')) {
+      try {
+        (window as any).adsbygoogle = (window as any).adsbygoogle || [];
+        (window as any).adsbygoogle.push({});
+      } catch (e) {
+        console.error('AdSense Error:', e);
+      }
+    }
+  }, []);
   return (
     <div className="intro-page">
       <FoodBanner />
@@ -27,6 +37,15 @@ const IntroPage: React.FC = () => {
         <button className="start-btn" onClick={() => navigate('/game')}>
           추천받기 시작
         </button>
+      </div>
+      <div className="intro-ad-wrapper">
+        <ins className="adsbygoogle"
+          style={{ display: 'block' }}
+          data-ad-client="ca-pub-5460335586767094"
+          data-ad-slot="5917717645"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        ></ins>
       </div>
       <AdvertiseBanner />
       <SiteInfo />
